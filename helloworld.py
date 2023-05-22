2fffb2b703137d4d7cf3f738d85f473589d2b2a4
AvrahamOmer <omeravraham25@gmail.com>
2023-05-22 18:52:18 +0300

    codewisper

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,11 @@
+import os
 """Module priting hello world."""
 print("hello-world")
+
+# create function to print the current path
+def print_path():
+    """Print the current path."""
+    print(os.getcwd())
+    return
+
+print_path()
