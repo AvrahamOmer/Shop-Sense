afa79985c18e4e4232ca8ff0581a3892386380d8
AvrahamOmer <omeravraham25@gmail.com>
2023-05-22 18:55:07 +0300

    linter

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,10 @@
-import os
 """Module priting hello world."""
+import os
 print("hello-world")
 
 # create function to print the current path
 def print_path():
-    """Print the current path."""
+    #Print the current path.
     print(os.getcwd())
-    return
 
 print_path()
